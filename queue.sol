00b68e74a229b8fb22bafb194182ae4bd28ef11f
Danila <peace_are_off@bk.ru>
2021-10-15 11:01:52 +0300

    The push function is completed

--- queue.sol
+++ queue.sol
@@ -3,5 +3,25 @@ pragma ton-solidity >= 0.35.0;
 pragma AbiHeader expire;
 
 contract queue {
-    
+    string[] queue;
+
+    constructor() public{
+        require(tvm.pubkey() != 0, 101);
+        require(msg.pubkey() == tvm.pubkey(), 102);
+        tvm.accept();
+    }
+
+    modifier modifyQueue {
+        require(msg.pubkey() == tvm.pubkey(), 102);
+        tvm.accept();
+        _;
+    }
+
+    function pushClient(string name) public modifyQueue{
+        queue.push(name);
+    }
+
+    function showQueue() public view returns(string[]) {
+        return queue;
+    }
 }
